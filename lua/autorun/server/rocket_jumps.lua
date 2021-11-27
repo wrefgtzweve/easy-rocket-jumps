f42a27d56d49d5f6722a14d2557d2a90486405fc
Florian Trayon <floriantrayon942@gmail.com>
2021-11-27 15:06:45 +0100

    Fixed update of convar values
    Under some conditions, changing values on some convars does not change the script behavior

--- lua/autorun/server/rocket_jumps.lua
+++ lua/autorun/server/rocket_jumps.lua
@@ -1,35 +1,20 @@
 -- Convars
-local enabled = CreateConVar( "rocketjumps_enabled", 1, { FCVAR_ARCHIVE }, "Enables rocket jumping (1/0).", 0 ):GetBool()
-cvars.AddChangeCallback( "rocketjumps_enabled", function( _, _, val )
-    enabled = tonumber( val )
-end)
-
-local scaleDamage = CreateConVar( "rocketjumps_dmgmult", 0.4, { FCVAR_ARCHIVE }, "The damage multiplier for self damaging explosions.", 0 ):GetFloat()
-cvars.AddChangeCallback( "rocketjumps_dmgmult", function( _, _, val )
-    scaleDamage = tonumber( val )
-end)
-
-local forceMult = CreateConVar( "rocketjumps_forcemult", 1, { FCVAR_ARCHIVE }, "The force multiplier for self damaging explosions.", 0 ):GetFloat()
-cvars.AddChangeCallback( "rocketjumps_forcemult", function( _, _, val )
-    forceMult = tonumber( val )
-end)
-
-local allExplosions = CreateConVar( "rocketjumps_allexplosions", 0, { FCVAR_ARCHIVE }, "Should the force multiplier be applied to all explosions instead of only self inflicted ones?.", 0 ):GetBool()
-cvars.AddChangeCallback( "rocketjumps_allexplosions", function( _, _, val )
-    allExplosions = tonumber( val )
-end)
+local enabled = CreateConVar( "rocketjumps_enabled", 1, { FCVAR_ARCHIVE }, "Enables rocket jumping (1/0).", 0 )
+local scaleDamage = CreateConVar( "rocketjumps_dmgmult", 0.4, { FCVAR_ARCHIVE }, "The damage multiplier for self damaging explosions.", 0 )
+local forceMult = CreateConVar( "rocketjumps_forcemult", 1, { FCVAR_ARCHIVE }, "The force multiplier for self damaging explosions.", 0 )
+local allExplosions = CreateConVar( "rocketjumps_allexplosions", 0, { FCVAR_ARCHIVE }, "Should the force multiplier be applied to all explosions instead of only self inflicted ones?.", 0 )
 
 local function reduceRocketDamage( ent, dmginfo )
-    if not enabled then return end
+    if not enabled:GetBool() then return end
     if not dmginfo:IsExplosionDamage() then return end
     if not ent:IsPlayer() then return end
 
     local attacker = dmginfo:GetAttacker()
 
-    if not allExplosions and attacker ~= ent then return end
+    if not allExplosions:GetBool() and attacker ~= ent then return end
 
     local dmgForce = dmginfo:GetDamageForce()
-    local newForce = dmgForce * forceMult
+    local newForce = dmgForce * forceMult:GetFloat()
     dmginfo:SetDamageForce( newForce )
 
     if ent:KeyDown( IN_DUCK	) then
@@ -38,7 +23,7 @@ local function reduceRocketDamage( ent, dmginfo )
         ent:SetVelocity( newForce / 70 )
     end
 
-    dmginfo:ScaleDamage( scaleDamage )
+    dmginfo:ScaleDamage( scaleDamage:GetFloat() )
 end
 
 hook.Add( "EntityTakeDamage", "rocketjumpsEntityTakeDamage", reduceRocketDamage )
